cc9e13aef636290c4d83417b13e211a3e507c128
Luke <lukemorton.dev@gmail.com>
2016-06-23 13:42:28 +0100

    Implement loadTodos action with test

--- client/actions/todos.js
+++ client/actions/todos.js
@@ -7,3 +7,9 @@ export const editTodo = createAction('edit todo')
 export const completeTodo = createAction('complete todo')
 export const completeAll = createAction('complete all')
 export const clearCompleted = createAction('clear complete')
+
+export const loadTodos = function () {
+  return (dispatch) => {
+    setTimeout(() => dispatch(addTodo('Use Redux with thunk')), 1000);
+  };
+};
---
+++ client/spec/data/loadingTodosSpec.js
@@ -0,0 +1,16 @@
+import { loadTodos } from '../../actions/todos'
+
+describe('Loading todos', function () {
+  describe('when no todos exist', function () {
+    it('should load todos', function (done) {
+      const store = mockStore({})
+
+      store.subscribe(function () {
+        expect(store.getActions()[0]).to.contain({ type: 'add todo' })
+        done()
+      })
+
+      store.dispatch(loadTodos())
+    });
+  });
+});
